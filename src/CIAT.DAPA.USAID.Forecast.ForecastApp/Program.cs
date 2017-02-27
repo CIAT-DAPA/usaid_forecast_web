c1ff3c3f1ad5b2134fcc6ca1d71cb39dca33c2fa
stevensotelo <stevenbetancurt@hotmail.com>
2017-02-27 14:26:44 -0500

    added forecast app

---
+++ src/CIAT.DAPA.USAID.Forecast.ForecastApp/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CIAT.DAPA.USAID.Forecast.ForecastApp
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+        }
+    }
+}
