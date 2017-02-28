e12faa54f967e6c637c5d1718ce0fad75b43a100
stevensotelo <stevenbetancurt@hotmail.com>
2017-02-27 19:15:25 -0500

    added method to export climate historical data in console app

---
+++ src/CIAT.DAPA.USAID.Forecast.ForecastApp/Controllers/COut.cs
@@ -0,0 +1,96 @@
+﻿using CIAT.DAPA.USAID.Forecast.Data.Database;
+using CIAT.DAPA.USAID.Forecast.Data.Enums;
+using CIAT.DAPA.USAID.Forecast.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CIAT.DAPA.USAID.Forecast.ForecastApp.Controllers
+{
+    /// <summary>
+    /// This Class export data from database
+    /// </summary>
+    public class COut
+    {
+        public const string PATH_STATES = "estacionesMensuales";
+        private ForecastDB db { get; set; }
+
+        /// <summary>
+        /// Method Construct
+        /// </summary>
+        public COut()
+        {
+            db = new ForecastDB(Program.cnn, Program.db);
+        }
+
+        /// <summary>
+        /// Method to export the historical data of the weather stations by states
+        /// </summary>
+        /// <param name="path">Path where the files will located</param>
+        /// <param name="measure">Measure to export</param>
+        /// <param name="start">Year to start</param>
+        /// <param name="end">Year to end</param>
+        public async Task<bool> exportStatesHistoricalAsync(string path, MeasureClimatic measure, int start, int end)
+        {
+            StringBuilder csv;
+            string header, line;
+            // Create directory
+            if (!Directory.Exists(path + COut.PATH_STATES))
+                Directory.CreateDirectory(path + COut.PATH_STATES);
+            var states = await db.state.listEnableAsync();
+            foreach (var s in states)
+            {
+                Console.WriteLine("Creating " + s.name);
+                csv = new StringBuilder();
+                var weather_stations = await db.weatherStation.listEnableByStateAsync(s.id);
+
+                // Create the header of file
+                header = "year,month,";
+                foreach (var ws in weather_stations)
+                    header += ws.id.ToString() + ",";
+                header = header.Substring(0, header.Length - 1);
+
+                // get historical climate data
+                var hc = await db.historicalClimatic.byWeatherStationsAsync(weather_stations.Select(p => p.id).Distinct().ToArray());
+                
+                // This code search by every year and month the data of every weather station
+                for(int y=start; y<=end; y++)
+                {
+                    for (int i = 1; i <= 12; i++)
+                    {
+                        line = y.ToString() + "," + i.ToString() + ",";
+                        foreach (var ws in weather_stations)
+                        {
+                            var data_year = hc.SingleOrDefault(p => p.year == y && p.weather_station == ws.id);
+                            if (data_year != null)
+                            {
+                                var data_month = data_year.monthly_data.SingleOrDefault(p => p.month == i);
+                                if (data_month != null)
+                                {
+                                    var data_measure = data_month.data.SingleOrDefault(p => p.measure == measure);
+                                    if (data_measure != null)
+                                        line += data_measure.value.ToString() + ",";
+                                    else
+                                        line += "0,";
+                                }
+                                else
+                                    line += "0,";
+                            }
+                            else
+                                line += "0,";
+                        }
+                        // Add line to file
+                        csv.AppendLine(line.Substring(0, line.Length - 1));
+                    }
+                }
+                // Create the physical file
+                File.WriteAllText(path + COut.PATH_STATES + @"\" + s.name + ".csv", header + "\n" + csv.ToString());
+            }
+            return true;
+        }
+        
+    }
+}
--- src/CIAT.DAPA.USAID.Forecast.ForecastApp/Program.cs
+++ src/CIAT.DAPA.USAID.Forecast.ForecastApp/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using CIAT.DAPA.USAID.Forecast.Data.Enums;
+using CIAT.DAPA.USAID.Forecast.ForecastApp.Controllers;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -7,8 +9,88 @@ namespace CIAT.DAPA.USAID.Forecast.ForecastApp
 {
     public class Program
     {
+        public static string cnn { get; set; }
+        public static string db { get; set; }
         public static void Main(string[] args)
         {
+            Program.cnn = "mongodb://localhost:27017";
+            Program.db = "forecast_db";
+            MainAsync(args).GetAwaiter().GetResult();
+            Console.WriteLine();
+            Console.WriteLine("Press Enter");
+            Console.ReadLine();
+        }
+
+        static async Task MainAsync(string[] args)
+        {
+            try
+            {
+                int path;
+                // Check the first parameter to validate if the action is export (-out) or import (-in)
+                if (Program.searchParameter(args, "-out") == 0)
+                {
+                    COut output = new COut();
+                    // Get 
+                    path = Program.searchParameter(args, "-p");
+                    Program.validateParameter(path, "-p");
+                    // Export states
+                    // -out -s "prec" -p "C:\Users\hsotelo\Desktop\test export\\" -start 1981 -end 2013
+                    int s = Program.searchParameter(args, "-s");
+                    if(s >= 0)
+                    {
+                        int start = Program.searchParameter(args, "-start");
+                        Program.validateParameter(start, "-start");
+                        int end = Program.searchParameter(args, "-end");
+                        Program.validateParameter(end, "-end");
+
+                        Console.WriteLine("Exporting states");
+                        await output.exportStatesHistoricalAsync(args[path + 1], (MeasureClimatic)Enum.Parse(typeof(MeasureClimatic), args[s + 1], true), int.Parse(args[start + 1]),int.Parse(args[end + 1]));
+                    }
+                }
+                else if (Program.searchParameter(args, "-in") == 0)
+                {
+
+                }
+                else if (Program.searchParameter(args, "-help") == 0)
+                {
+                    Console.WriteLine("Help Menu");
+                }
+                else
+                {
+                    throw new Exception("The first parameter should indicate if you want export (-out) or import (-in).\nRecommend execute with parameter -help to get more information");
+                }
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.WriteLine(ex.StackTrace);
+            }
+        }
+
+        /// <summary>
+        /// Method that search a value into parameters
+        /// </summary>
+        /// <param name="args">Arguments list</param>
+        /// <param name="name">Name of parameter</param>
+        /// <returns>If the answer is -1, the paramter doesn't exist, otherwise, represents the index of paramater</returns>
+        public static int searchParameter(string[] args, string name)
+        {
+            for (int i = 0; i < args.Length; i++)
+                if (args[i].Trim().ToLower().Equals(name.Trim().ToLower()))
+                    return i;
+            return -1;
+        }
+
+        /// <summary>
+        /// Method that validates the input parameter
+        /// </summary>
+        /// <param name="index">Position in the args</param>
+        /// <param name="name">Name of parameter</param>
+        public static void validateParameter(int index, string name)
+        {
+            if (index < 0)
+                throw new Exception("The parameter " + name + " is mandatory");
         }
     }
 }
