fd9548b162622548fdc998f5912f277ce23c3517
stevensotelo <stevenbetancurt@hotmail.com>
2017-03-08 13:49:00 -0500

    delete de read line in the console app

--- src/CIAT.DAPA.USAID.Forecast.ForecastApp/Controllers/CIn.cs
+++ src/CIAT.DAPA.USAID.Forecast.ForecastApp/Controllers/CIn.cs
@@ -341,7 +341,6 @@ namespace CIAT.DAPA.USAID.Forecast.ForecastApp.Controllers
             }
 
             Console.WriteLine("Forecast imported");
-            Console.ReadLine();
             return true;
         }
     }
