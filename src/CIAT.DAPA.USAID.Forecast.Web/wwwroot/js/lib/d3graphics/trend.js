f6c4c11b81b2c9bd661250dd89d12b051a16126d
stevensotelo <stevenbetancurt@hotmail.com>
2017-03-09 07:49:34 -0500

    fixed the trend graphic. It now has colors and style

--- src/CIAT.DAPA.USAID.Forecast.Web/wwwroot/js/lib/d3graphics/trend.js
+++ src/CIAT.DAPA.USAID.Forecast.Web/wwwroot/js/lib/d3graphics/trend.js
@@ -72,27 +72,31 @@ Trend.prototype.render = function () {
         .y1(function (d) { return y(d.data.perc_5); });
 
     this.base.svg.append('path')
-        .attr('class', 'area upper outer')
+        //.attr('class', 'area upper outer')
+        .attr('class', 'trend_yield_area_outer')
         .attr('d', upperOuterArea)
         .attr('clip-path', 'url(#trend_clip)');
 
     this.base.svg.append('path')
-        .attr('class', 'area lower outer')
+        //.attr('class', 'area lower outer')
+        .attr('class', 'trend_yield_area_outer')
         .attr('d', lowerOuterArea)
         .attr('clip-path', 'url(#trend_clip)');
 
     this.base.svg.append('path')
-        .attr('class', 'area upper inner')
+        //.attr('class', 'area upper inner')
+        .attr('class', 'trend_yield_area_inner')
         .attr('d', upperInnerArea)
         .attr('clip-path', 'url(#trend_clip)');
 
     this.base.svg.append('path')
-        .attr('class', 'area lower inner')
+        //.attr('class', 'area lower inner')
+        .attr('class', 'trend_yield_area_inner')
         .attr('d', lowerInnerArea)
         .attr('clip-path', 'url(#trend_clip)');
 
     this.base.svg.append('path')
-        .attr('class', 'median-line')
+        .attr('class', 'trend_yield_median_line')
         .attr('d', medianLine)
         .attr('clip-path', 'url(#trend_clip)');
 
