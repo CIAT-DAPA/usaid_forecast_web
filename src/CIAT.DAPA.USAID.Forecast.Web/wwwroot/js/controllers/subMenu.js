ff4dc66f34b4e262121fe617f8363e2b3b20589e
crios <carios1usb@gmail.com>
2017-04-11 15:57:34 -0500

    Working on sub-sections in climate section

---
+++ src/CIAT.DAPA.USAID.Forecast.Web/wwwroot/js/controllers/subMenu.js
@@ -0,0 +1,55 @@
+﻿'use strict';
+
+/**
+ * @ngdoc function
+ * @name ForecastApp.controller:ClimateCtrl
+ * @description
+ * # ClimateCtrl
+ * Controller of the ForecastApp
+ */
+angular.module('ForecastApp')
+  .controller('SubMenuCtrl', function ($scope, config,tools) {
+      
+      $scope.type = tools.source();
+
+      if ($scope.type === 'climate') {
+          $scope.subSections = [
+          {
+              name: 'Predicción climática', value: 'prob'
+          },
+          {
+              name: 'Precipitación', value: 'prec'
+          },
+          {
+              name: 'Temperatura', value: 't_max'
+          },
+          {
+              name: 'Radiación solar', value: 'sol_rad'
+          }
+          ];
+      }
+      else if ($scope.type === 'crop') {
+          $scope.subSections = config.yield_default_var;
+      }
+      else {
+
+      }
+
+      $scope.renderView = function ($value, $index) {
+          console.log($value);
+          console.log($index);
+          var climate_content = $("#containerBlock");
+          if ($value === 'prob') {
+              $(".sectionTitle").text("Predicción Climática");
+              $.get('/Clima/Forecast', function (data) {
+                  climate_content.html(data);
+              });
+          }
+          else {
+              $.get('/Clima/ClimateVars', function (data) {
+                  climate_content.html(data);
+              });
+
+          }
+      }
+  });
