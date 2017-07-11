21e6c5330517509266595b70c37c104d960bea74
stevensotelo <stevenbetancurt@hotmail.com>
2017-07-11 11:14:53 -0500

    changed the main menu. Now the buttons (weather, rice and maiz) have the geographic information below them

---
+++ src/CIAT.DAPA.USAID.Forecast.WebAPI/Models/Entities/CropGeographicEntity.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CIAT.DAPA.USAID.Forecast.WebAPI.Models.Entities
+{
+    /// <summary>
+    /// This class has all attributes that are allowed search by the API
+    /// about crop geographic entity
+    /// </summary>
+    public class CropGeographicEntity
+    {
+        public string id { get; set; }
+        public string name { get; set; }
+        public List<StateEntity> states { get; set; }
+    }
+}
