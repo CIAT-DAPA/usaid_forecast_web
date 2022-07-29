8670b299d99d276231749774308908afb1e92575
stevensotelo <stevenbetancurt@hotmail.com>
2022-07-29 18:58:16 -0500

    added legend to maps in the indicators section

--- src/CIAT.DAPA.USAID.Forecast.Web/Models/Indicators/IndicatorModel.cs
+++ src/CIAT.DAPA.USAID.Forecast.Web/Models/Indicators/IndicatorModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -39,6 +40,14 @@ namespace CIAT.DAPA.USAID.Forecast.Web.Models.Indicators
         /// Get or set the indicators' units
         /// </summary>
         public string Units { get; set; }
+        /// <summary>
+        /// Get or set the min value of the indicator
+        /// </summary>
+        public double Min { get; set; }
+        /// <summary>
+        /// Get or set the max value of the indicator
+        /// </summary>
+        public double Max { get; set; }
 
         /// <summary>
         /// Method constructor
@@ -47,14 +56,23 @@ namespace CIAT.DAPA.USAID.Forecast.Web.Models.Indicators
         public IndicatorModel(string line)
         {
             string[] fields = line.Split(",");
-            Crop = fields[0];
-            CropID = fields[1];
-            Group = fields[2];
-            GroupID = fields[3];
-            IndicatorName = fields[4];
-            IndicatorNameID = fields[5];
-            Description = fields[6];
-            Units = fields[7];
+            try
+            {
+                Crop = fields[0];
+                CropID = fields[1];
+                Group = fields[2];
+                GroupID = fields[3];
+                IndicatorName = fields[4];
+                IndicatorNameID = fields[5];
+                Description = fields[6];
+                Units = fields[7];
+                Min = double.Parse(fields[8], CultureInfo.InvariantCulture);
+                Max = double.Parse(fields[9], CultureInfo.InvariantCulture);
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
         }
     }
 }
--- src/CIAT.DAPA.USAID.Forecast.Web/wwwroot/js/map/maps_indicators.js
+++ src/CIAT.DAPA.USAID.Forecast.Web/wwwroot/js/map/maps_indicators.js
@@ -11,14 +11,55 @@ var geoserver_workspace;
   * @param {any} id id div
   * @param {any} idx position of the map list
   */
-function plot_map(id, idx) {
+function plot_map(id, idx, min, max, group) {
     maps[idx] = L.map(id).setView([conf.latitude, conf.longitude], conf.zoom);
     L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
         attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>',
         subdomains: ['a', 'b', 'c']
     }).addTo(maps[idx]);
 
+    // Adding event click on map. It shows the value of each pixel
     maps[idx].on('click', onMapClick);
+
+    // 
+    var legend = L.control({ position: 'bottomleft' });
+
+    legend.onAdd = function (map) {
+        var div = L.DomUtil.create('div', 'info legend');
+        div.innerHTML = '<strong>Categories</strong><br />';
+        categories = generatePoints(min, max, 10);
+        for (var i = 0; i < categories.length; i++) {
+            div.innerHTML += '<i class="circle" style="background:' + getColor(i, group) + '"></i> ' +
+                (i == 0 ? '< ' + parseInt(categories[i]) :
+                    (i == (categories.length - 1) ? '> ' + parseInt(categories[i] + 1) :
+                    parseInt(categories[i] + 1) + ' - ' + parseInt(categories[i + 1]))) +
+                "<br />";
+        }
+        //div.innerHTML = labels.join('<br />');
+        return div;
+    };
+    legend.addTo(maps[idx]);
+}
+
+const generatePoints = (startingNumber, endingNumber, maxPoints) => Array.from(
+    { length: maxPoints },
+    (_, i) => startingNumber + i * parseInt((endingNumber - startingNumber) / (maxPoints - 1))
+) 
+
+/**
+ * Method that returns the color regarding to index and type
+ * @param {any} index
+ * @param {any} type
+ */
+function getColor(index, type) {
+    var scales = {
+        heavy_rainfall: ['#f7fcf0', '#e0f3db', '#ccebc5', '#a8ddb5', '#7bccc4', '#4eb3d3', '#2b8cbe', '#0868ac', '#084081', '#08306b'],
+        drought: ['#a50026', '#d73027', '#f46d43', '#fdae61', '#fee090', '#e0f3f8', '#abd9e9', '#74add1', '#4575b4', '#313695'],
+        dry_spells: ['#ffffe5', '#fff7bc', '#fee391', '#fec44f', '#fe9929', '#ec7014', '#cc4c02', '#993404', '#7f2704', '#662506'],
+        heat_waves: ['#ffffcc', '#ffeda0', '#fed976', '#feb24c', '#fd8d3c', '#fc4e2a', '#e31a1c', '#bd0026', '#800026', '#67000d']
+    }
+    //console.log(index + ' ' + type + ' ' + scales[type][index]);
+    return scales[type][index];
 }
 
 /**
@@ -135,7 +176,7 @@ function load_maps() {
     // Loading maps
     layers_selected.forEach((value, idx) => {
         // ploting maps
-        plot_map('map_' + idx, idx);
+        plot_map('map_' + idx, idx, value.min, value.max, value.groupID);
         // Adding layer
         plot_layer(idx, value.cropID + "_" + value.indicatorID, time);
     });
