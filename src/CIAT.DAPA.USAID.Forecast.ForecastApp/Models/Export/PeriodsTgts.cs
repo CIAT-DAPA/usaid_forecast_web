f18fb6e477271e53d45c9cc8f6fb2293ad0a574c
santiagoC <s.calderon@cgiar.org>
2022-10-20 10:39:35 -0500

    add period model

---
+++ src/CIAT.DAPA.USAID.Forecast.ForecastApp/Models/Export/PeriodsTgts.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CIAT.DAPA.USAID.Forecast.ForecastApp.Models.Export
+{
+    public class PeriodsTgts
+    {
+        public string months { get; set; }
+        public string year { get; set; }
+
+    }
+}
